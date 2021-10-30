6f17f3e082423f2068b368ba5d3a7312da9b2865
maiavil <90734493+maiavil@users.noreply.github.com>
2021-10-30 00:51:43 -0300

    Add files via upload

---
+++ analisissinestadistica.py
@@ -0,0 +1,64 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Oct 30 00:15:43 2021
+
+@author: Maia
+"""
+
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Oct 29 22:39:24 2021
+
+@author: Maia
+"""
+
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.optimize import curve_fit 
+os.chdir (r'C:\Users\54116\Desktop\Labo 4\lock in II\Largo1')
+data = np.loadtxt("5k.40k.txt", delimiter = " ",skiprows=1)
+#%%
+
+frec=data[:,0]
+r=data[:,1]
+theta=data[:,2]
+x=data[:,3]
+y=data[:,4]
+#%%
+eyi=0.002*y
+eys=erry
+errory=np.sqrt(eyi**2+eys**2)
+
+exi=0.002*x
+exs=xstd/np.sqrt(20)
+errorx=np.sqrt(exi**2+exs**2)
+
+X=y/x
+
+eX=np.sqrt((errory/x)**2+(errorx*y/x**2)**2)
+
+plt.errorbar(frec,X,yerr=eX, linestyle='none')
+
+#%%
+plt.plot(frec,np.arctan(theta))
+
+#%%
+X=y/x
+def f1(w,C):
+    return C*w
+param, pcov = curve_fit(f1, frec, X, sigma=eX) 
+print(param)
+print(np.sqrt(np.diag(pcov)))
+l=np.linspace(5000,40000,100000)
+plt.plot(l, [f1(i,param[0]) for i in l])
+plt.errorbar(frec,X,yerr=eX, marker='.', linestyle='none')
+#%%
+def f2(w,k):
+    return k*w
+param, pcov = curve_fit(f2, frec, y, sigma=errory) 
+print(param)
+print(np.sqrt(np.diag(pcov)))
+#%%
+erry=np.mean(errory)
+errX=np.mean(eX)
